96f5a2245c79d54c69f6123bebbe9e1e99face15
Gaurav <gauravkhatri492006@gmail.com>
2026-08-15 11:59:22 +0530

    Added new cont in teachers API

--- backend/src/controllers/teacherController.js
+++ backend/src/controllers/teacherController.js
@@ -41,6 +41,24 @@ const getAllTeachers = async (req, res) => {
     }
 };
 
+const getTeacherById = async (req, res) => {
+    try{
+        const teacher = await Teacher.findById(req.params.id);
+
+        if(!teacher){
+            return res.status(404).json({
+                message: "Teacher not found"
+            });
+        }
+
+        res.status(200).json(teacher);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
     registerTeacher,
     getAllTeachers
