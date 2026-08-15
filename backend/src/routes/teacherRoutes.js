76a2e8dbfb353c6ce6f10ddf4200d1b193c23650
Gaurav <gauravkhatri492006@gmail.com>
2026-08-15 09:09:29 +0530

    Updated require const in teachers Routes

--- backend/src/routes/teacherRoutes.js
+++ backend/src/routes/teacherRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerTeacher } = require("../controllers/teacherController");
+const {
+    registerTeacher,
+    getAllTeachers
+} = require("../controllers/teacherController");
 
 router.post("/register", registerTeacher);
+
+router.get("/", getAllTeachers);
 
 module.exports = router;
