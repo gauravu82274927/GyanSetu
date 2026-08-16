22698a4d461d1d399fe3ab3a9558e2747437b270
Gaurav <gauravkhatri492006@gmail.com>
2026-08-16 09:23:12 +0530

    Added delete in Routes

--- backend/src/routes/teacherRoutes.js
+++ backend/src/routes/teacherRoutes.js
@@ -3,11 +3,13 @@ const {
     registerTeacher,
     getAllTeachers,
     getTeacherById,
-    updateTeacher
+    updateTeacher,
+    deleteTeacher
 } = require("../controllers/teacherController");
 const router = express.Router();
 router.post("/register", registerTeacher);
 router.get("/", getAllTeachers);
 router.get("/:id", getTeacherById);
 router.put("/:id", updateTeacher);
+router.delete("/:id", deleteTeacher);
 module.exports = router;
