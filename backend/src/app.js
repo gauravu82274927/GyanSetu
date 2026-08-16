9ad992f5598bb03ce7dc5afd8986e7d419025b00
Gaurav <gauravkhatri492006@gmail.com>
2026-08-16 13:55:16 +0530

    Connected submission to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const teacherRoutes = require("./routes/teacherRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const assignmentRoutes = require("./routes/AssignmentRoutes");
+const submissionRoutes = require("./routes/SubmissionRoutes");
 const app = express();
 
 app.use(express.json());
 app.use("/api/teachers", teacherRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/assignments", assignmentRoutes);
+app.use("/api/submissions", submissionRoutes);
 
 module.exports = app;
