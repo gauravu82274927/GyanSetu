d286c6e863dc4ea9caaa82fbf2159ed28847e3e0
Gaurav <gauravkhatri492006@gmail.com>
2026-08-16 20:22:06 +0530

    Installed bcryptjs and jsonwebtoken and added password field in Teacher model

--- backend/src/models/Teacher.js
+++ backend/src/models/Teacher.js
@@ -8,6 +8,10 @@ const teacherSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required: true
+    },
     subjects: {
         type: [String],
         required: true
