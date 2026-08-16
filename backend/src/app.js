f826e54d97fdcd5771658dbd75e425da061ee27b
Gaurav <gauravkhatri492006@gmail.com>
2026-08-16 09:35:24 +0530

    Connected Student Routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const teacherRoutes = require("./routes/teacherRoutes");
+const studentRoutes = require("./routes/studentRoutes");
 const app = express();
 
 app.use(express.json());
 app.use("/api/teachers", teacherRoutes);
+app.use("/api/students", studentRoutes);
 
 module.exports = app;
