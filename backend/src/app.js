2d8dfbba0f64abe995253c01809698fbf8346eed
Gaurav <gauravkhatri492006@gmail.com>
2026-08-16 09:54:09 +0530

    Connected Assignment routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const teacherRoutes = require("./routes/teacherRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const assignmentRoutes = require("./routes/AssignmentRoutes");
 const app = express();
 
 app.use(express.json());
 app.use("/api/teachers", teacherRoutes);
 app.use("/api/students", studentRoutes);
+app.use("/api/assignments", assignmentRoutes);
 
 module.exports = app;
